ad5a79ae3155e042f72d5886463515a56c53b70f
Yehor Borkov <yehor.borkov@gmail.com>
2025-11-24 18:01:37 +0000

    refactor: move create env into function

--- native/scripts/install_torch.sh
+++ native/scripts/install_torch.sh
@@ -37,23 +37,29 @@ install_vai_conda_channel() {
     fi
 }
 
-if [[ ${VAI_CONDA_CHANNEL} =~ .*"tar.gz" ]]; then
-    install_vai_conda_channel
-    export VAI_CONDA_CHANNEL="file://${channel_dir}"
-fi
+create_pytorch_env() {
+    set +x
+    source "${CONDA_PREFIX}/conda/etc/profile.d/conda.sh"
+    source "${CONDA_PREFIX}/conda/etc/profile.d/mamba.sh"
+    set -x
 
-set +x
-source "${CONDA_PREFIX}/conda/etc/profile.d/conda.sh"
-source "${CONDA_PREFIX}/conda/etc/profile.d/mamba.sh"
-set -x
+    torchvision_cmd="pip install torch==1.13.1+${TORCH_CUDA_VERSION} torchvision==0.14.1+${TORCH_CUDA_VERSION} --index-url https://download.pytorch.org/whl/${TORCH_CUDA_VERSION}"
+    mamba env create -v -f "${VAI_ROOT}/native/gpu_conda/vitis-ai-pytorch.yml"
+    conda activate vitis-ai-pytorch
+    pip install --force-reinstall scipy numpy==1.22 protobuf==3.20.3 tensorboard graphviz==0.19.1 imageio scikit-image natsort nibabel easydict yacs fire numba loguru
+    mkdir -p $VAI_ROOT/compiler
+    conda activate vitis-ai-pytorch
+    $torchvision_cmd
+    cp -r $CONDA_PREFIX/lib/python3.8/site-packages/vaic/arch $VAI_ROOT/compiler/arch
+    conda clean -y --force-pkgs-dirs
+    conda config --env --remove-key channels
+}
 
-torchvision_cmd="pip install torch==1.13.1+${TORCH_CUDA_VERSION} torchvision==0.14.1+${TORCH_CUDA_VERSION} --index-url https://download.pytorch.org/whl/${TORCH_CUDA_VERSION}"
-mamba env create -v -f "${VAI_ROOT}/native/gpu_conda/vitis-ai-pytorch.yml"
-conda activate vitis-ai-pytorch
-pip install --force-reinstall scipy numpy==1.22 protobuf==3.20.3 tensorboard graphviz==0.19.1 imageio scikit-image natsort nibabel easydict yacs fire numba loguru
-mkdir -p $VAI_ROOT/compiler
-conda activate vitis-ai-pytorch
-$torchvision_cmd
-cp -r $CONDA_PREFIX/lib/python3.8/site-packages/vaic/arch $VAI_ROOT/compiler/arch
-conda clean -y --force-pkgs-dirs
-conda config --env --remove-key channels
+install_torch() {
+    if [[ ${VAI_CONDA_CHANNEL} =~ .*"tar.gz" ]]; then
+        install_vai_conda_channel
+        export VAI_CONDA_CHANNEL="file://${channel_dir}"
+    fi
+}
+
+"$@"
