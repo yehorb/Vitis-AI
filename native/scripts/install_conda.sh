2a6e0332343a24fe008062c5281c2b9822e49913
Yehor Borkov <yehor.borkov@gmail.com>
2025-11-24 16:40:08 +0000

    refactor: move conda isntallation into function

--- native/scripts/install_conda.sh
+++ native/scripts/install_conda.sh
@@ -5,16 +5,14 @@ set -ex
 CONDA_PREFIX="${CONDA_PREFIX:-/home/xilinx}"
 MINIFORGE_VERSION="${MINIFORGE_VERSION:-"25.9.1-0"}"
 
-if [[ ${DOCKER_TYPE} =~ .*'rocm'* && ${TARGET_FRAMEWORK} =~ .*"pytorch" ]]; then
-    ln -s /opt/conda $CONDA_PREFIX/conda
-else
+install_conda() {
     cd /tmp &&
         wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/Miniforge3-${MINIFORGE_VERSION}-Linux-x86_64.sh &&
         /bin/bash ./miniconda.sh -b -p $CONDA_PREFIX/conda &&
         . $CONDA_PREFIX/conda/etc/profile.d/conda.sh &&
         rm -fr /tmp/miniconda.sh &&
         /$CONDA_PREFIX/conda/bin/conda clean -y --force-pkgs-dirs
-fi
 
-echo ". $CONDA_PREFIX/conda/etc/profile.d/conda.sh" >>~vitis-ai-user/.bashrc
-sudo ln -s $CONDA_PREFIX/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+    echo ". $CONDA_PREFIX/conda/etc/profile.d/conda.sh" >>~vitis-ai-user/.bashrc
+    sudo ln -s $CONDA_PREFIX/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+}
