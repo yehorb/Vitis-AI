3c2c6ad4fc3223f78dec46382729aa2fb8d1ec04
Yehor Borkov <yehor.borkov@gmail.com>
2025-11-24 19:24:07 +0000

    refactor: streamline basic pip requirements

--- native/scripts/install_torch.sh
+++ native/scripts/install_torch.sh
@@ -50,12 +50,28 @@ create_pytorch_env() {
         --yes
 
     conda activate vitis-ai-pytorch
-    pip install --force-reinstall scipy numpy==1.22 protobuf==3.20.3 tensorboard graphviz==0.19.1 imageio scikit-image natsort nibabel easydict yacs fire numba loguru
     mkdir -p $VAI_ROOT/compiler
     conda activate vitis-ai-pytorch
     torchvision_cmd="pip install torch==1.13.1+${TORCH_CUDA_VERSION} torchvision==0.14.1+${TORCH_CUDA_VERSION} --index-url https://download.pytorch.org/whl/${TORCH_CUDA_VERSION}"
     $torchvision_cmd
     cp -r $CONDA_PREFIX/lib/python3.8/site-packages/vaic/arch $VAI_ROOT/compiler/arch
+
+    pip install \
+        --force-reinstall \
+        scipy \
+        numpy==1.22 \
+        protobuf==3.20.3 \
+        tensorboard \
+        graphviz==0.19.1 \
+        imageio \
+        scikit-image \
+        natsort \
+        nibabel \
+        easydict \
+        yacs \
+        fire \
+        numba \
+        loguru
 }
 
 install_torch() {
