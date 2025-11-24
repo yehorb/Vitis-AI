5c9c359c3f2cacc0640aa8602c12c530b73c457a
Yehor Borkov <yehor.borkov@gmail.com>
2025-11-24 16:56:59 +0000

    refactor: streamline native conda install

--- native/scripts/install_conda.sh
+++ native/scripts/install_conda.sh
@@ -2,17 +2,35 @@
 
 set -ex
 
+DOWNLOAD_DIR="${DOWNLOAD_DIR:-/tools/Xilinx/Downloads}"
 CONDA_PREFIX="${CONDA_PREFIX:-/home/xilinx}"
 MINIFORGE_VERSION="${MINIFORGE_VERSION:-"25.9.1-0"}"
 
-install_conda() {
-    cd /tmp &&
-        wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/Miniforge3-${MINIFORGE_VERSION}-Linux-x86_64.sh &&
-        /bin/bash ./miniconda.sh -b -p $CONDA_PREFIX/conda &&
-        . $CONDA_PREFIX/conda/etc/profile.d/conda.sh &&
-        rm -fr /tmp/miniconda.sh &&
-        /$CONDA_PREFIX/conda/bin/conda clean -y --force-pkgs-dirs
-
-    echo ". $CONDA_PREFIX/conda/etc/profile.d/conda.sh" >>~vitis-ai-user/.bashrc
-    sudo ln -s $CONDA_PREFIX/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+download_miniforge() {
+    wget --progress=dot:mega \
+        -O "${DOWNLOAD_DIR}/Miniforge3.sh" \
+        "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/Miniforge3-${MINIFORGE_VERSION}-$(uname)-$(uname -m).sh"
 }
+
+install_miniforge() {
+    "${DOWNLOAD_DIR}/Miniforge3.sh" -b -p $CONDA_PREFIX/conda
+}
+
+install_conda() {
+    if [[ ! -f "${DOWNLOAD_DIR}/Miniforge3.sh" ]]; then
+        download_miniforge
+        # Executable, read-only
+        chmod a+x,a-w "${DOWNLOAD_DIR}/Miniforge3.sh"
+    fi
+
+    if [[ ! -d "${CONDA_PREFIX}/conda" ]]; then
+        install_miniforge
+    fi
+}
+
+activate_conda() {
+    source "${CONDA_PREFIX}/conda/etc/profile.d/conda.sh"
+    source "${CONDA_PREFIX}/conda/etc/profile.d/mamba.sh"
+}
+
+"$@"
