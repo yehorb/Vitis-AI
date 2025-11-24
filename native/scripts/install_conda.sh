f0c5840300aad0d34095d33aeb60a4fcc191aa80
Yehor Borkov <yehor.borkov@gmail.com>
2025-11-24 16:37:12 +0000

    refactor: set up conda installation variables

--- native/scripts/install_conda.sh
+++ native/scripts/install_conda.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 set -ex
+
+CONDA_PREFIX="${CONDA_PREFIX:-/home/xilinx}"
+MINIFORGE_VERSION="${MINIFORGE_VERSION:-"25.9.1-0"}"
+
 if [[ ${DOCKER_TYPE} =~ .*'rocm'* && ${TARGET_FRAMEWORK} =~ .*"pytorch" ]]; then
-    ln -s /opt/conda $VAI_ROOT/conda
+    ln -s /opt/conda $CONDA_PREFIX/conda
 else
     export HOME=~vitis-ai-user
 
@@ -15,11 +19,11 @@ else
     export MINIFORGE_VERSION="25.3.1-0"
     cd /tmp &&
         wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/Miniforge3-${MINIFORGE_VERSION}-Linux-x86_64.sh &&
-        /bin/bash ./miniconda.sh -b -p $VAI_ROOT/conda &&
-        . $VAI_ROOT/conda/etc/profile.d/conda.sh &&
+        /bin/bash ./miniconda.sh -b -p $CONDA_PREFIX/conda &&
+        . $CONDA_PREFIX/conda/etc/profile.d/conda.sh &&
         rm -fr /tmp/miniconda.sh &&
-        /$VAI_ROOT/conda/bin/conda clean -y --force-pkgs-dirs
+        /$CONDA_PREFIX/conda/bin/conda clean -y --force-pkgs-dirs
 fi
 
-echo ". $VAI_ROOT/conda/etc/profile.d/conda.sh" >>~vitis-ai-user/.bashrc
-sudo ln -s $VAI_ROOT/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+echo ". $CONDA_PREFIX/conda/etc/profile.d/conda.sh" >>~vitis-ai-user/.bashrc
+sudo ln -s $CONDA_PREFIX/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
