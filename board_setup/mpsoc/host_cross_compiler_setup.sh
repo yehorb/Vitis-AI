2336460db539b76f10b5f3b2149e5a8c9e04e9d4
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-02 18:25:28 +0000

    refactor: cleanup and streamline board setup script

--- board_setup/mpsoc/host_cross_compiler_setup.sh
+++ board_setup/mpsoc/host_cross_compiler_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright 2021 Xilinx Inc.
 #
@@ -12,36 +13,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#!/bin/bash
 
+set -ex
 
-wget https://www.xilinx.com/bin/public/openDownload?filename=sdk-2022.2.0.0.sh -O sdk-2022.2.0.0.sh
-wget https://www.xilinx.com/bin/public/openDownload?filename=vitis_ai_2022.2-r3.0.0.tar.gz -O vitis_ai_2022.2-r3.0.0.tar.gz
+DOWNLOAD_DIR="${DOWNLOAD_DIR:-/home/xilinx/Downloads}"
+INSTALL_PATH="${INSTALL_PATH:-${HOME}/petalinux_sdk_2022.2}"
+SDK_URL="${SDK_URL:-https://www.xilinx.com/bin/public/openDownload?filename=sdk-2022.2.0.0.sh}"
+VITIS_AI_URL="${VITIS_AI_URL:-https://www.xilinx.com/bin/public/openDownload?filename=vitis_ai_2022.2-r3.0.0.tar.gz}"
 
-chmod +x sdk-2022.2.0.0.sh
+sdk_file="${DOWNLOAD_DIR}/sdk-2022.2.0.0.sh"
+vitis_ai_file="${DOWNLOAD_DIR}/vitis_ai_2022.2-r3.0.0.tar.gz"
+sysroot_path="${INSTALL_PATH}/sysroots/cortexa72-cortexa53-xilinx-linux"
 
-echo "The Cross Compiler will be installed in ~/petalinux_sdk_2022.2 by default"
-install_path=~/petalinux_sdk_2022.2
+download_sdk() {
+    if [[ ! -f "${sdk_file}" ]]; then
+        wget --progress=dot:mega \
+            -O "${sdk_file}" \
+            "${SDK_URL}"
+        chmod a+x,a-w "${sdk_file}"
+    fi
+}
 
-if [ -d $install_path ]
-then
-echo ""
-else
-mkdir -p $install_path
-fi
+download_vitis_ai() {
+    if [[ ! -f "${vitis_ai_file}" ]]; then
+        wget --progress=dot:mega \
+            -O "${vitis_ai_file}" \
+            "${VITIS_AI_URL}"
+        chmod a-w "${vitis_ai_file}"
+    fi
+}
 
-echo $install_path|./sdk-2022.2.0.0.sh
+install_sdk() {
+    if [[ ! -d "${INSTALL_PATH}" ]]; then
+        mkdir -p "${INSTALL_PATH}"
+    fi
+    echo "${INSTALL_PATH}" | "${sdk_file}"
+}
 
-rm -r $install_path/sysroots/cortexa72-cortexa53-xilinx-linux/usr/share/cmake/XRT/
+install_vitis_ai() {
+    rm -rf "${sysroot_path}/usr/share/cmake/XRT/"
+    tar -xzvf "${vitis_ai_file}" -C "${sysroot_path}/"
+}
 
-tar -xzvf vitis_ai_2022.2-r3.0.0.tar.gz -C $install_path/sysroots/cortexa72-cortexa53-xilinx-linux/
+print_instructions() {
+    set +x
+    echo "Complete Cross Compiler installation"
+    echo ""
+    echo "Please run the following command to enable Cross Compiler"
+    echo "    source ${INSTALL_PATH}/environment-setup-cortexa72-cortexa53-xilinx-linux"
+    echo "If you run the above command failed, run the following commands to enable Cross Compiler"
+    echo "    unset LD_LIBRARY_PATH"
+    echo "    source ${INSTALL_PATH}/environment-setup-cortexa72-cortexa53-xilinx-linux"
+    echo ""
+}
 
-echo "Complete Cross Compiler installation"
-echo ""
-echo "Please run the following command to enable Cross Compiler"
-echo "    source $install_path/environment-setup-cortexa72-cortexa53-xilinx-linux"
-echo "If you run the above command failed, run the following commands to enable Cross Compiler"
-echo "    unset LD_LIBRARY_PATH"
-echo "    source $install_path/environment-setup-cortexa72-cortexa53-xilinx-linux"
-echo ""
+install_all() {
+    download_sdk
+    download_vitis_ai
+    install_sdk
+    install_vitis_ai
+    print_instructions
+}
+
+"$@"
