ba8bb2505ff0df790b214d4898589eb0d0850d6f
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-08 18:07:25 +0000

    fix: correctly initialize cross-compiler in run_compile

--- examples/vai_runtime/yolox_nano/code/run_compile.sh
+++ examples/vai_runtime/yolox_nano/code/run_compile.sh
@@ -1,3 +1,5 @@
+unset LD_LIBRARY_PATH
+source /home/xilinx/tools/petalinux_sdk_2022.2/environment-setup-cortexa72-cortexa53-xilinx-linux
 vai_c_xir \
     -x build/var/quantized_stft/YOLOX_0_int.xmodel \
     -a "${CONDA_PREFIX}/lib/python3.8/site-packages/vaic/arch/DPUCZDX8G/KV260/arch.json" \
