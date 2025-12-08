6b80a6e1f1ce5b9d779e9280ea359c3f3b91b147
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-08 17:18:29 +0000

    fix: make quant tool input_channels aware

--- examples/vai_runtime/yolox_nano/code/tools/quant.py
+++ examples/vai_runtime/yolox_nano/code/tools/quant.py
@@ -160,7 +160,11 @@ def main(exp, args, num_gpu):
         args.batch_size = 1
 
     model = exp.get_model()
-    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+    logger.info(
+        "Model Summary: {}".format(
+            get_model_info(model, exp.test_size, getattr(exp, "input_channels", 3))
+        )
+    )
     logger.info("Model Structure:\n{}".format(str(model)))
 
     evaluator = exp.get_evaluator(args.batch_size, is_distributed, args.test, args.legacy)
