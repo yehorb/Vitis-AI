3ceae6ffb06b2c77516be211d90b95e5ae055263
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-09 09:53:39 +0000

    refactor: eagerly copy all *.npy (saved numpy arrays - test objects) to a board

--- examples/vai_runtime/yolox_nano/code/setup_board.sh
+++ examples/vai_runtime/yolox_nano/code/setup_board.sh
@@ -2,6 +2,5 @@ ssh petalinux@kria.local "mkdir -p /home/petalinux/models/yolox_stft"
 scp -O build/var/quantized_stft/target/yolox_stft_kv260.xmodel \
     src/edge/inference.py \
     data/stft/20251207_162413/meta.json \
-    build/var/quantized_stft/test.npy \
-    build/var/quantized_stft/test_labels.npy \
+    build/var/quantized_stft/*.npy \
     petalinux@kria.local:/home/petalinux/models/yolox_stft/
