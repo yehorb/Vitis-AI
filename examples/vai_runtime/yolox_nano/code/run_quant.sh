9e12ec8ae37b581df181ffe4d5c8888871266cfb
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-09 09:54:04 +0000

    refactor: clean up a single command example in run_quant

--- examples/vai_runtime/yolox_nano/code/run_quant.sh
+++ examples/vai_runtime/yolox_nano/code/run_quant.sh
@@ -9,7 +9,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and 
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 export CUDA_VISIBLE_DEVICES=0
@@ -32,23 +32,12 @@ python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --con
 # dump xmpdel
 python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR} --is_dump
 
-# W_QUANT=1 python -m yolox.tools.quant \
+# CUDA_VISIBLE_DEVICES=0 W_QUANT=1 CUDA_HOME=/usr/local/cuda python \
+#     -m yolox.tools.quant \
 #     -f code/exps/example/custom/yolox_nano_stft_relu.py \
 #     -c YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth \
+#     -b 32 \
+#     -d 1 \
+#     --conf 0.001 \
 #     --quant_mode test \
-#     --quant_dir build/var/quantized_stft \
-#     -b 8
-# W_QUANT=1 python -m yolox.tools.quant \
-#     -f code/exps/example/custom/yolox_nano_stft_relu_q.py \
-#     -c YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth \
-#     --quant_mode test \
-#     --quant_dir build/var/quantized_stft \
-#     --is_dump \
-#     -b 1
-# W_QUANT=1 python -m yolox.tools.quant \
-#     -f code/exps/example/custom/yolox_nano_stft_relu.py \
-#     -c YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth \
-#     --quant_mode test \
-#     --quant_dir build/var/quantized_stft \
-#     --is_dump \
-#     -b 1
+#     --quant_dir build/var/quantized_stft
