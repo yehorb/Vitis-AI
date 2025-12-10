39ce40c8b60fe9045e54193f4b25a50ee50d543b
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-10 17:19:34 +0000

    fix: remove env_setup.sh

--- examples/vai_runtime/yolox_nano/env_setup.sh
+++
@@ -1,7 +0,0 @@
-echo "Install all the python dependencies using pip"
-pip install --trusted-host xcdpython.xilinx.com -r requirements_ptq.txt
-
-ROOT_DIR=${PWD}
-cd ${ROOT_DIR}/code
-pip install -v -e .
-cd ${ROOT_DIR}
