518403d137377efff1d6522217a9690c9ae7c0e4
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-10 10:41:18 +0000

    refactor: update eval parameters

--- examples/vai_runtime/yolox_nano/code/run_eval.sh
+++ examples/vai_runtime/yolox_nano/code/run_eval.sh
@@ -17,7 +17,9 @@ export CUDA_VISIBLE_DEVICES=0
 GPU_NUM=1
 
 export LD_LIBRARY_PATH=${PWD}/code:${LD_LIBRARY_PATH}
-BATCH=32
+BATCH=2048
 CFG=code/exps/example/custom/yolox_nano_stft_relu.py
 CKPT=./YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth
-python -m yolox.tools.eval -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001
+
+set -ex
+python -m yolox.tools.eval -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.25
