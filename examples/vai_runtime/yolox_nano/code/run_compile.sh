d9f8c31c65943f69ae77d7e2acb7b5ccc6dabd60
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-11 17:06:04 +0000

    refactor: update compilation and deployment scripts

--- examples/vai_runtime/yolox_nano/code/run_compile.sh
+++ examples/vai_runtime/yolox_nano/code/run_compile.sh
@@ -1,7 +1,9 @@
+Q_DIR="${Q_DIR:-build/var/yolox_tiny_stft_quantized}"
+
 unset LD_LIBRARY_PATH
 source /home/xilinx/tools/petalinux_sdk_2022.2/environment-setup-cortexa72-cortexa53-xilinx-linux
 vai_c_xir \
-    -x build/var/quantized_stft/YOLOX_0_int.xmodel \
+    -x "${Q_DIR}/YOLOX_0_int.xmodel" \
     -a "${CONDA_PREFIX}/lib/python3.8/site-packages/vaic/arch/DPUCZDX8G/KV260/arch.json" \
-    -o build/var/quantized_stft/target \
-    -n yolox_stft_kv260
+    -o "${Q_DIR}/target" \
+    -n kv260
--- examples/vai_runtime/yolox_nano/code/setup_board.sh
+++ examples/vai_runtime/yolox_nano/code/setup_board.sh
@@ -1,6 +1,16 @@
-ssh petalinux@kria.local "mkdir -p /home/petalinux/models/yolox_stft"
-scp -O build/var/quantized_stft/target/yolox_stft_kv260.xmodel \
+set -ex
+
+T_DIR="${T_DIR:-/home/petalinux/models/yolox_tiny/}"
+Q_DIR="${Q_DIR:-build/var/yolox_tiny_stft_quantized}"
+
+if [ -z "${STFT_DATASET}" ]; then
+    echo "[${0}]: \$STFT_DATASET is not set"
+    exit 1
+fi
+
+ssh petalinux@kria.local "mkdir -p ${T_DIR}"
+scp -O "${Q_DIR}/target/kv260.xmodel" \
     src/edge/inference.py \
-    data/stft/20251207_162413/meta.json \
-    build/var/quantized_stft/*.npy \
-    petalinux@kria.local:/home/petalinux/models/yolox_stft/
+    "${STFT_DATASET}/meta.json" \
+    "${Q_DIR}/*.npy" \
+    "petalinux@kria.local:${T_DIR}"
