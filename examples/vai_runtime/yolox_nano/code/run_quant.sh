ee426a2982113ecd69ac67f32f02688d48be9c06
Yehor Borkov <yehor.borkov@gmail.com>
2025-12-11 16:31:21 +0000

    fix: update quant script to use new model

--- examples/vai_runtime/yolox_nano/code/run_quant.sh
+++ examples/vai_runtime/yolox_nano/code/run_quant.sh
@@ -19,25 +19,17 @@ export W_QUANT=1
 export CUDA_HOME=/usr/local/cuda
 
 BATCH=32
-CFG=code/exps/example/custom/yolox_nano_stft_relu.py
-CKPT=YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth
-Q_DIR=build/var/quantized_stft
+CFG=code/exps/example/custom/yolox_tiny_stft_relu.py
+CKPT=YOLOX_outputs/yolox_tiny_stft_relu/best_ckpt.pth
+Q_DIR=build/var/yolox_tiny_stft_quantized
+
+set -ex
 
 MODE='calib'
-python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR}
+python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --nms 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR}
 
 MODE='test'
-python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR}
+python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --nms 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR}
 
 # dump xmpdel
-python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR} --is_dump
-
-# CUDA_VISIBLE_DEVICES=0 W_QUANT=1 CUDA_HOME=/usr/local/cuda python \
-#     -m yolox.tools.quant \
-#     -f code/exps/example/custom/yolox_nano_stft_relu.py \
-#     -c YOLOX_outputs/yolox_nano_stft_relu/best_ckpt.pth \
-#     -b 32 \
-#     -d 1 \
-#     --conf 0.001 \
-#     --quant_mode test \
-#     --quant_dir build/var/quantized_stft
+python -m yolox.tools.quant -f ${CFG} -c ${CKPT} -b ${BATCH} -d ${GPU_NUM} --conf 0.001 --nms 0.001 --quant_mode ${MODE} --quant_dir ${Q_DIR} --is_dump
